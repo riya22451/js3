b328d48b3754434124dac75a29e08085a9e8747e
riya22451 <riyaaggarwal226@gmail.com>
2024-12-04 11:16:56 +0000

    operations in js

---
+++ 01_basic/conversion.js
@@ -0,0 +1,48 @@
+let score=null
+let valueInNumber=Number(score);
+console.log(typeof(number))
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1,false=>0
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=>true,o=>false
+//""=>false
+//"riya"=>true
+//**************** operations ************************
+let value=3;
+let negValue=-value
+console.log(negValue);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+let str1="hello"
+let str2="hitesh"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(true)
+console.log(+true)
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.table([num1,num2,num3])
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
