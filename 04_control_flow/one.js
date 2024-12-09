81efe400235fead32047ac86f56af0b94d6ac861
riya22451 <riyaaggarwal226@gmail.com>
2024-12-09 15:50:11 +0000

    for loop learning

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+//if statement
+  const isUserLoggedIn=true
+  const temperature=40
+
+
+if(temperature<50){
+  console.log("less than 50")
+}else{
+console.log("greater than 50")
+}
+
+const score=200
+if(score>100){
+    const power="fly"
+    console.log(`${power}`)
+}
+
+
+const balance=1000
+if(balance<1000) console.log("balance is less than 1000");
+    
+
